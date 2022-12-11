ceb61fb31b16d1fb7883c673d5214f94c612c129
AntonioMarinCamuna <antoniomarincamuna1@gmail.com>
2022-12-11 13:03:59 +0100

    Commit and push

--- app/src/main/java/com/example/music4ll/MainActivity.java
+++ app/src/main/java/com/example/music4ll/MainActivity.java
@@ -25,20 +25,21 @@ public class MainActivity extends AppCompatActivity {
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     /*
-    * Modificación del onClick del botón "btnLogin" para que actué en función de lo introducido
+    * Modificación del onClick del botón "btnLogin" para que actué en función de las credenciales
+    * introducidas.
     */
     View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
 
             /*
-            * Comparación que hace una cosa u otra si la información introducida es correcta o no
+            * Comparación que hace una cosa u otra si la información introducida es correcta o no.
             */
             if(usr.getText().toString().equals("admin") && psw.getText().toString().equals("1234")){
 
                 /*
                 * Código encargado de instanciar un intent con el activity "ActivityReproductor"
-                * y encargado de comenzar la actividad
+                * y encargado de comenzar la actividad.
                 */
                 Intent i = new Intent(MainActivity.this, ActivityReproductor.class);
                 startActivity(i);
@@ -47,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
                 /*
                 * Código que actúa cuando los datos no son correctos y que muestra un fragmento de
-                * ERROR
+                * ERROR.
                 */
                 FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
                 FragmentoError fe = new FragmentoError();
@@ -55,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
 
                 /*
                 * Forzamos al programa que borre lo introducido en los campos "usr" y "pwd" para que
-                * al volver del fragmento estén vacíos
+                * al volver del fragmento estén vacíos.
                 */
                 usr.setText("");
                 psw.setText("");
@@ -67,30 +68,30 @@ public class MainActivity extends AppCompatActivity {
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     /*
-    * Modificación del "onCreate"
+    * Modificación del "onCreate".
     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().hide(); //Código encargado de eliminar la barra superior de las aplicaciones
+        getSupportActionBar().hide(); //Código encargado de eliminar la barra superior de las aplicaciones.
 
         /*
         * Instanciamos cada una de las variables globales definidas antes y les asignamos los
-        * recursos pertinentes
+        * recursos pertinentes.
         */
         usr = findViewById(R.id.userField);
         psw = findViewById(R.id.passwordField);
         btnLogin = findViewById(R.id.button);
         img = findViewById(R.id.imgLogo);
 
-        btnLogin.setBackgroundColor(Color.BLACK); //Modificación estética para el botón (color negro)
+        btnLogin.setBackgroundColor(Color.BLACK); //Modificación estética para el botón (color negro).
 
-        img.setVisibility(View.VISIBLE); //Código que pone la imagen de la portada VISIBLE
+        img.setVisibility(View.VISIBLE); //Código que pone la imagen de la portada VISIBLE.
 
         btnLogin.setOnClickListener(listener); //Código que usa la variable listener obtenida en el @override
-                                               //del onClickListener
+                                               //del onClickListener.
 
     }
 }
