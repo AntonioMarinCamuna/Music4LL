24998a2e36708853092f154b1e5c9333a8b46ff2
AntonioMarinCamuna <antoniomarincamuna1@gmail.com>
2022-12-11 12:22:25 +0100

    Update

--- app/src/main/java/com/example/music4ll/MainActivity.java
+++ app/src/main/java/com/example/music4ll/MainActivity.java
@@ -14,6 +14,9 @@ import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+    * Definición global de cierta variables que se van a usar
+    */
     TextView usr;
     TextView psw;
     Button btnLogin;
@@ -21,21 +24,39 @@ public class MainActivity extends AppCompatActivity {
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    /*
+    * Modificación del onClick del botón "btnLogin" para que actué en función de lo introducido
+    */
     View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
 
+            /*
+            * Comparación que hace una cosa u otra si la información introducida es correcta o no
+            */
             if(usr.getText().toString().equals("admin") && psw.getText().toString().equals("1234")){
 
+                /*
+                * Código encargado de instanciar un intent con el activity "ActivityReproductor"
+                * y encargado de comenzar la actividad
+                */
                 Intent i = new Intent(MainActivity.this, ActivityReproductor.class);
                 startActivity(i);
 
             }else{
 
+                /*
+                * Código que actúa cuando los datos no son correctos y que muestra un fragmento de
+                * ERROR
+                */
                 FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
                 FragmentoError fe = new FragmentoError();
                 ft.replace(android.R.id.content,fe).addToBackStack(null).commit();
 
+                /*
+                * Forzamos al programa que borre lo introducido en los campos "usr" y "pwd" para que
+                * al volver del fragmento estén vacíos
+                */
                 usr.setText("");
                 psw.setText("");
 
@@ -45,22 +66,31 @@ public class MainActivity extends AppCompatActivity {
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    /*
+    * Modificación del "onCreate"
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().hide(); //Código encargado de eliminar la barra superior de las aplicaciones
+
+        /*
+        * Instanciamos cada una de las variables globales definidas antes y les asignamos los
+        * recursos pertinentes
+        */
         usr = findViewById(R.id.userField);
         psw = findViewById(R.id.passwordField);
         btnLogin = findViewById(R.id.button);
-        btnLogin.setBackgroundColor(Color.BLACK);
-
-        getSupportActionBar().hide();
-
         img = findViewById(R.id.imgLogo);
-        img.setVisibility(View.VISIBLE);
 
-        btnLogin.setOnClickListener(listener);
+        btnLogin.setBackgroundColor(Color.BLACK); //Modificación estética para el botón (color negro)
+
+        img.setVisibility(View.VISIBLE); //Código que pone la imagen de la portada VISIBLE
+
+        btnLogin.setOnClickListener(listener); //Código que usa la variable listener obtenida en el @override
+                                               //del onClick
 
     }
 }
